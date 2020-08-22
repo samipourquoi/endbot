aa29c641ad0430afd298c06c4cf4592d3bf69d5d
samipourquoi <sami.guiderdoni@gmail.com>
2020-08-22 21:16:01 +0200

    Rewrite part of the Rcon code to be able to send multiple packets in a short amount of time without throwing errors.

--- src/rcon/Rcon.js
+++ src/rcon/Rcon.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const Net = require("net");
+const net = require("net");
+
 const Packet = require("./Packet");
 
 /**
@@ -16,9 +17,12 @@ class Rcon {
 
 		this.client = client;
 
+		this.queue = [];
+		this.draining = false;
+
 		let connect = () => {
 			console.log(`Connecting to ${this.name} Rcon...`);
-			this.connection = Net.createConnection({
+			this.connection = net.createConnection({
 				host: this.host,
 				port: this.port
 			}, () => {
@@ -41,7 +45,7 @@ class Rcon {
 		let packet = new Packet(this.generateId(), 3, this.password);
 		this.connection.write(packet.buffer);
 
-		let promise = new Promise((resolve, reject) => {
+		return new Promise((resolve, reject) => {
 			this.connection.once("data", data => {
 				let response = Packet.read(data);
 				if (response.id != packet.id) reject(`Couldn't authenticate to ${this.name} Rcon`);
@@ -50,18 +54,43 @@ class Rcon {
 				resolve(`Running ${this.name} Rcon`);
 			});
 		});
+	}
 
-		return promise;
+	/**
+	 * Drains the pending queue.
+	 * This is used to be able to send multiple
+	 * packets in a small amount of time without
+	 * throwing an error.
+	 */
+	drain() {
+		if (this.draining) return;
+
+		// Recursion messes up the "this", so we must use
+		// something like that to be able to use it.
+		let dis = this;
+		let workerDrain = () => {
+			if (dis.queue.length > 0) {
+				dis.draining = true;
+				let packet = dis.queue.shift();
+				dis.connection.write(packet.buffer, workerDrain);
+			} else {
+				dis.draining = false;
+			}
+		};
+		workerDrain();
 	}
 
 	sendCommand(command) {
 		let packet = new Packet(this.generateId(), 2, command);
-		this.connection.write(packet.buffer);
+		this.queue.push(packet);
+		this.drain();
 
 		return new Promise((resolve, reject) => {
-			this.connection.once("data", data => {
-				resolve(Packet.read(data));
+			this.connection.on("data", data => {
+				data = Packet.read(data);
+				if (data.id == packet.id) resolve(data);
 			});
+
 			setTimeout(() => {
 				reject("Timeout exceeded");
 			}, this.timeout);
