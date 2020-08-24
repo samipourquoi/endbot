5ad58971098a290760bb44b25c6b87a902cf39b3
samipourquoi <sami.guiderdoni@gmail.com>
2020-08-24 03:12:37 +0200

    Add online --all command

--- src/commands/discord/Online.js
+++ src/commands/discord/Online.js
@@ -7,12 +7,22 @@ class Online extends Command {
 		super(client);
 		this.info = {
 			"name": "Online",
-			"usage": "online",
+			"usage": "online [--all]",
 			"description": "Sends the list of all online players across servers"
 		};
 	}
 
-	async run(message) {
+	async run(message, args) {
+		if (args[0] == "--all") {
+			let iterator = this.client.bridges.entries();
+			for (let i = 0; i < this.client.bridges.size; i++) {
+				let bridge = iterator.next().value[1];
+				console.log(bridge);
+				await message.channel.send(await this.getOnlinePlayers(bridge.rcon));
+			}
+			return;
+		}
+
 		let servers = this.client.config.servers;
 		let keys = Object.keys(servers);
 		for (let i = 0; i < keys.length; i++) {
@@ -20,30 +30,34 @@ class Online extends Command {
 
 			if (server["bridge-channel"] == message.channel.id) {
 				let rcon = this.client.bridges.get(message.channel.id).rcon;
-				let data = await rcon.sendCommand("list");
-				let response = data.body.split(" ");
-				let online = {
-					onlineCount: response[2],
-					maxCount: response[6],
-					players: response.slice(9)
-				};
-
-				if (online.players[0] == "") {
-					online.players = "No online players";
-				}
-
-				let embed = this.client.createEmbed("result")
-					.setTitle("Online players on " + rcon.name)
-					.addField(`${online.onlineCount}/${online.maxCount}`, online.players);
-
-				message.channel.send(embed);
+				await message.channel.send(await this.getOnlinePlayers(rcon));
 				return;
 			}
 		}
 
-		// TODO: Add global online, not only in bridge channels
-
 		await message.channel.send(this.client.errorEmbed("bridge"));
+
+		// TODO: Add global online, not only in bridge channels
+	}
+
+	async getOnlinePlayers(rcon) {
+		let data = await rcon.sendCommand("list");
+		let response = data.body.split(" ");
+		let online = {
+			onlineCount: response[2],
+			maxCount: response[6],
+			players: response.slice(9)
+		};
+
+		if (online.players[0] == "") {
+			online.players = "No online players";
+		}
+
+		let embed = this.client.createEmbed("result")
+			.setTitle("Online players on " + rcon.name)
+			.addField(`${online.onlineCount}/${online.maxCount}`, online.players);
+
+		return embed;
 	}
 
 	toString() {
