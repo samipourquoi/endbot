f0717922e285a649ae96bf704e3dec5a7743a08e
samipourquoi <sami.guiderdoni@gmail.com>
2020-10-04 21:30:50 +0200

    Fix help command (server)

--- src/commands/server/Help.js
+++ src/commands/server/Help.js
@@ -15,13 +15,16 @@ class Help extends ServerCommand {
 	run(rcon) {
 		let help = [];
 
+		let addedCommands = []; // To prevent duplicates
 		let commands = this.client.serverCommands;
 		let keys = Object.keys(commands);
 		for (let i = 0; i < keys.length; i++) {
-			let info = commands[keys[i]].info;
-			let name = `{"text":"${info.name}","color":"gray","hoverEvent":{"action":"show_text","value":"${info.description}"}}`;
-			let usage = `{"text":": ${info.usage}", "color": "white"}`;
+			let command = commands[keys[i]];
+			if (addedCommands.includes(command.description)) continue;
+			let name = `{"text":"${command.name}","color":"gray","hoverEvent":{"action":"show_text","value":"${command.description}"}}`;
+			let usage = `{"text":": ${command.usage}", "color": "white"}`;
 			help.push(`${name}, ${usage}`);
+			addedCommands.push(command.description);
 		}
 
 		let message = "[\"\"," + help.join(",\"\\n\",") + "]";
