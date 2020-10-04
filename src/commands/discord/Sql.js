ee4a587cb8fc0f9aeb08ba92d3301e191fe84a11
samipourquoi <sami.guiderdoni@gmail.com>
2020-10-04 18:33:43 +0200

    Add 'sql' command

---
+++ src/commands/discord/Sql.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const Command = require("../Command.js");
+
+class Sql extends Command {
+	constructor(client) {
+		super(client);
+		this.info = {
+			"name": "SQL",
+			"usage": "sql",
+			"description": "Do SQL queries to the database"
+		};
+	}
+
+	async run(message, args) {
+		if (!message.member.roles.cache.has(this.client.config["op-role"])) {
+			await message.channel.send("da fuck you tryin' to do");
+			return;
+		}
+		
+		this.client.db.async_all(args.join(" ")).then(result => {
+			message.channel.send(`\`\`\`JSON\n${JSON.stringify(result, null, "\t")}\`\`\``, { split: true });
+		}).catch(error => {
+			message.channel.send(`\`\`\`diff\n-${error}\`\`\``);
+		});
+	}
+
+	toString() {
+		return this.info;
+	}
+}
+
+module.exports = Sql;
