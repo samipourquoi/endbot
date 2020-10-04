1005e8c1d361fb94afd19da75d78c04c627c32da
samipourquoi <sami.guiderdoni@gmail.com>
2020-10-04 21:14:31 +0200

    Add getter functions to get info about a command

--- src/commands/Command.js
+++ src/commands/Command.js
@@ -3,6 +3,23 @@
 class Command {
 	constructor(client) {
 		this.client = client;
+		this.info = {
+			"name": "Command Template",
+			"usage": "cmd",
+			"description": "Template of a command description"
+		};
+	}
+	
+	get name() {
+		return this.info.name;
+	}
+	
+	get usage() {
+		return this.info.usage;
+	}
+	
+	get description() {
+		return this.info.description;
 	}
 }
 
