990eb161d63cfc0948bd2e9a7bc4c5866d5bc04d
samipourquoi <sami.guiderdoni@gmail.com>
2020-10-06 19:37:45 +0200

    Add 'links add' command

--- modules/discord_links/index.js
+++ modules/discord_links/index.js
@@ -2,7 +2,7 @@
 
 function setup(client) {
 	// Creates table in database
-	client.db.async_run("CREATE TABLE IF NOT EXISTS discord_links (invite TEXT);");
+	client.db.async_run("CREATE TABLE IF NOT EXISTS discord_links (name TEXT, invite TEXT, emote_id TEXT);");
 }
 
 module.exports = {
--- modules/discord_links/src/discord/Links.js
+++ modules/discord_links/src/discord/Links.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const Command = require("@root/src/commands/Command");
+const Discord = require("discord.js");
+const EMOTE_SERVER_ID = "694311004258959390";
+const Canvas = require("canvas");
 
 class Links extends Command {
 	constructor(client) {
@@ -10,9 +13,73 @@ class Links extends Command {
 			"usage": "links add",
 			"description": "Adds discord invitations to an embed"
 		};
+		this.emoteServer = this.client.guilds.cache.get(EMOTE_SERVER_ID);
 	}
 		
-	run(message, args) {
+	async run(message, args) {
+		try {			
+			switch (args[0]) {
+			case "add": 	await this.add(message, args.slice(1)); break;
+			case "remove": 	this.remove(message, args.slice(1)); 	break;
+			case "publish": this.publish(message, args.slice(1)); 	break;
+			case "list": 	this.list(message, args.slice(1));	 	break;
+			}
+		} catch (e) {
+			let errorEmbed = this.client.createEmbed("error").setTitle(e);
+			message.channel.send(errorEmbed);
+		}
+	}
+	
+	async add(message, args) {
+		let inviteText = args[0];
+		if (!inviteText) throw "You must provide an invite link!";
+		
+		let invite = await this.client.fetchInvite(inviteText);
+		let name = invite.guild.name;
+		let emote = await this.createEmote(invite.guild);
+	
+		await this.client.db.async_run("INSERT or REPLACE INTO discord_links VALUES (?, ?, ?)", { params: [name, invite.url, emote.identifier] });
+		let responseEmbed = this.client.createEmbed("result").setTitle(`Successfully added ${name}! <:${emote.identifier}>`)
+		message.channel.send(responseEmbed);
+	}
+	
+	async createEmote(guild) {
+		let emoteName = guild.name.toLowerCase()
+							.replace(/[^\w ]/g, "")
+							.replace(/ /g, "_");
+		let iconURL = guild.iconURL({ format: "png" });
+		
+		let canvas = Canvas.createCanvas(128, 128);
+		let context = canvas.getContext("2d");
+		let image = await Canvas.loadImage(iconURL);
+		context.beginPath();
+
+		// How to draw a rounded Rectangle on HTML Canvas?
+		// https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-on-html-canvas
+		context.moveTo(128, 128)
+		context.arcTo(0, 128, 0, 0, 40);
+		context.arcTo(0, 0, 128, 0, 40);
+		context.arcTo(128, 0, 128, 128, 40);
+		context.arcTo(128, 128, 0, 128, 40);
+		context.clip();
+		
+		context.drawImage(image, 0, 0, 128, 128);
+		context.closePath();
+		
+		let buffer = canvas.toDataURL();
+		return this.emoteServer.emojis.create(buffer, emoteName);
+	}
+	
+	remove(message, args) {
+		
+	}
+	
+	publish(message, args) {
+		
+	}
+	
+	list(message, args) {
+		
 	}
 }
 
