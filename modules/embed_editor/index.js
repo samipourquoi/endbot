4d3d0bf88c547945e71c0f13c1ae7ddf41160642
samipourquoi <sami.guiderdoni@gmail.com>
2020-10-07 16:18:38 +0200

    Move Discord Links module to Embed Editor

--- modules/embed_editor/index.js
+++ modules/embed_editor/index.js
@@ -1,7 +1,21 @@
 "use strict";
 
+function setup(client) {
+	// Creates table in database
+	client.db.async_run(`CREATE TABLE IF NOT EXISTS discord_links (
+		name TEXT,
+		registry TEXT,
+		invite TEXT, 
+		emote_id TEXT,
+		PRIMARY KEY (name, registry)
+	);`);
+}
+
 module.exports = {
 	package: "Embed Editor Command",
+	
 	discord: "src/discord",
 	server: undefined,
+	
+	setup: setup,
 };
