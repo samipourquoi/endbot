01b692dca8dbba31f80f51b9c5ffa86cdd19fdf5
iDarkLightning <nirjhor.nath@gmail.com>
2020-11-30 14:29:24 -0500

    Added database table and init command

--- modules/projects/index.js
+++ modules/projects/index.js
@@ -3,5 +3,24 @@
 module.exports = {
 	package: "Projects System",
 	discord: "src/discord",
-	server: "src/server"
+	server: "src/server",
+	config: {
+		"category-id": "783019338814324787"
+	},
+
+	setup: async client => {
+		await client.db.async_run(`
+			CREATE TABLE IF NOT EXISTS projects (
+				name TEXT,
+				type TEXT,
+				description TEXT,
+				channel_id TEXT UNIQUE,
+				leaders TEXT,
+				members TEXT,
+				coords TEXT,
+				schematic TEXT,
+				mat_list TEXT
+			);
+		`);
+	}
 };
