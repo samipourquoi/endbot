3e99ac76751711b9e3e2f9b048c33d6dbaeba476
samipourquoi <sami.guiderdoni@gmail.com>
2020-12-20 14:51:03 +0100

    Fix a query that was making the bot create a fuckton of channels, you fucking fucker

--- modules/applications/index.js
+++ modules/applications/index.js
@@ -30,7 +30,7 @@ module.exports = {
 		// Database
 		// await client.db.async_run("CREATE TABLE IF NOT EXISTS tickets (id TEXT UNIQUE, applicant TEXT, link TEXT, pfp TEXT, discriminator INT, date TEXT);");
 		// await client.db.async_run("CREATE TABLE IF NOT EXISTS archived_tickets (id TEXT, name TEXT, round INTEGER, messages JSON NOT NULL, status TEXT, pfp TEXT DEFAULT '/assets/default-avatar.png', discriminator INT, date TEXT);");
-		await client.db.async_run("CREATE TABLE IF NOT EXISTS archived_logged_on (token TEXT, expires_in INT, pfp TEXT);");
+		await client.db.async_run("CREATE TABLE IF NOT EXISTS archived_logged_on (token TEXT, expires_in BIGINT, pfp TEXT);");
 		await client.db.async_run("INSERT IGNORE INTO settings VALUES ('total_applications', 0);");
 
 		await client.db.async_run(`
