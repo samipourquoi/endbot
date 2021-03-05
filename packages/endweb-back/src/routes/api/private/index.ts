7a800af02232503a4b9320b31db522c6809fd2c3
samipourquoi <sami.guiderdoni@gmail.com>
2021-03-05 22:37:48 +0100

    endweb: require auth. when accessing /api/private/* URIs

--- packages/endweb-back/src/routes/api/private/index.ts
+++ packages/endweb-back/src/routes/api/private/index.ts
@@ -1,5 +1,17 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { Auth } from "../../../auth";
 
 module.exports = Router()
+	.all("/*", isAuthed)
 	.use("/form", require("./form"))
 	.use("/apps", require("./apps"))
+
+async function isAuthed(req: Request, res: Response, next: NextFunction) {
+	const token = req.header("Authorization")
+	if (token && await Auth.isLoggedIn(token)) {
+		next();
+	} else {
+		res.status(401)
+			.end();
+	}
+}
