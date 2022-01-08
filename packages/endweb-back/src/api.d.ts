5f2c6d6421640afbcea3851cce3630b7e750e8ac
Syntro42 <syntro42@gmail.com>
2022-01-08 00:09:47 -0600

    project: refactor project to only use one main package
    At this moment, I don't have any desire to work on a website, so I am
    reconfiguring this project to only contain endbot. In the future, if a
    website is wanted, it should not be too much of a hassle to refactor the
    project into different packages.

--- packages/endweb-back/src/api.d.ts
+++
@@ -1,6 +0,0 @@
-import { Schemas } from "endbot/dist/database";
-import { ArchiveMessage } from "endbot/dist/structures/archive";
-
-export type GETAppInfo = Schemas.TicketAttributes & Schemas.ApplicantAttributes;
-
-export type GETChannel = ArchiveMessage[];
