2161e846835c3abc1116fa4d69098e905a6d21e9
Syntro42 <syntro42@gmail.com>
2022-01-10 18:21:07 -0600

    project: slightly adjust linting and formatting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,19 @@ module.exports = {
 		es2021: true,
 		node: true,
 	},
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-	],
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		ecmaVersion: 2021,
 		sourceType: "module",
 	},
-	plugins: [
-		"@typescript-eslint",
-		"prettier",
-	],
+	plugins: ["@typescript-eslint", "prettier"],
 	rules: {
 		"no-duplicate-imports": ["error", { includeExports: true }],
 		"no-unreachable-loop": "error",
 
 		// The following are formatting rules
-		"brace-style": ["error", "allman", { allowSingleLine: true }],
+		"brace-style": ["error", "1tbs", { allowSingleLine: true }],
 		"comma-dangle": [
 			"error",
 			{
