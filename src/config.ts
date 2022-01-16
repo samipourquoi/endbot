63d62f5731fb1d847152d1086ad7b5da4a44e9a8
Syntro42 <syntro42@gmail.com>
2022-01-16 09:23:32 -0600

    endbot: Handle when a config file is not found

--- src/config.ts
+++ src/config.ts
@@ -9,12 +9,19 @@ export class Config {
 	}
 
 	private init() {
-		const config = this.readConfigFile();
+		const config = this.readConfigFile("config.yml");
 		this.token = config.token;
 	}
 
-	private readConfigFile() {
-		const config = readFileSync("config.yml", "utf-8");
-		return YAML.parse(config)
+	private readConfigFile(file: string) {
+		try {
+			const config = readFileSync(file, "utf-8");
+			return YAML.parse(config);
+		} catch {
+			console.log(
+				"Please make a configuration file before starting the bot"
+			);
+			process.exit();
+		}
 	}
 }
